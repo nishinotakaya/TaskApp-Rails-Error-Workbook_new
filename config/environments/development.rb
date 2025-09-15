f804df37938f872015862bbdd91b461bcb71ce18
nishino <takaya@gmail.com>
2025-09-15 22:49:52 +0900

    chore: force sync docker-compose.yml & development.rb from main

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Use a file watcher to detect changes in source code,
+  # routes, locales, etc. This works better in Docker containers.
+  config.file_watcher = ActiveSupport::FileUpdateChecker
+  
+  # Force reload on every request in development (for Docker)
+  config.reload_classes_only_on_change = false
 end
