d6847809b749131da7dad791e7a067e22e7e3cdf
nishino <takaya@gmail.com>
2025-09-17 00:44:01 +0900

    routeとdb/migrate/20190705071427_create_tasks.rb修正

--- db/migrate/20190705071427_create_tasks.rb
+++ db/migrate/20190705071427_create_tasks.rb
@@ -1,12 +1,13 @@
-class Tasks < ActiveRecord::Migration[5.1]
+class CreateTasks < ActiveRecord::Migration[5.1]
   def change
-    create_table :task do |t|
+    create_table :tasks do |t|
       t.string :name
       t.text :description
       t.references :user, foreign_key: true
 
       t.timestamps
     end
+
     add_index :tasks, [:user_id, :created_at]
   end
 end
