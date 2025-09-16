ca53ff6dced7e4f04b43bbb296aed9c1fcd5c017
nishino <takaya@gmail.com>
2025-09-17 01:48:10 +0900

    chore: force sync docker-compose.yml & development.rb from main

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -48,17 +48,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use a file watcher to detect changes in source code,
-  # routes, locales, etc. This works better in Docker containers.
-  # config.file_watcher = ActiveSupport::FileUpdateChecker
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  # Force reload on every request in development (for Docker)
+  # Docker環境ではファイル監視が不安定なため、常にリロードするように設定
   config.reload_classes_only_on_change = false
   
-  # Enable live reload for Docker development
-  config.force_ssl = false
-  
-  # Disable bootsnap cache in development for better file watching
-  config.bootsnap = false if defined?(Bootsnap)
+  # Docker環境でのコードリロードを確実にするため、追加設定
+  config.after_initialize do
+    Rails.application.reloader.check!
+  end
+
 end
