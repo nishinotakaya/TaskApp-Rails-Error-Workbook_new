bdbbaf8fd44bae69ac898499712e283a4680fc7e
nishino <takaya@gmail.com>
2025-09-17 00:30:09 +0900

    chore: force sync docker-compose.yml & development.rb from main

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   # Force reload on every request in development (for Docker)
-  config.reload_classes_only_on_change = false
+  config.reload_classes_only_on_change = true
   
   # Enable live reload for Docker development
   config.force_ssl = false
