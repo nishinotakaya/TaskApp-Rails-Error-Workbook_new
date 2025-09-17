7ae3ed9f6f447397d051650472578792d15dd102
nishino <takaya@gmail.com>
2025-09-17 23:33:55 +0900

    chore: force sync docker-compose.yml & development.rb from main

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def top
-    　　
+    Rails.logger.warn("### CTRL_HIT #{Time.now.to_f}")
+    render plain: "controller hit #{Time.now.to_i}"
   end
 end
