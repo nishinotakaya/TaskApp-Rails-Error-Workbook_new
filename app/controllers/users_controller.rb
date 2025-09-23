128c05df11f8f39e576cfc92608a1987768632c9
koki-kato <2k2s.work@gmail.com>
2025-09-23 20:04:43 +0900

    ActiveModel::ForbiddenAttributesError対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(params[:user])
+    if @user.update(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
